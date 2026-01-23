3013c337e7c3791ea0cd2c2b1f7b6fd4b429cd3d
Vanuja Karunaratne <vanujakofficial@gmail.com>
2026-01-23 22:29:18 +0530

    Jenkins changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ import hearingsRouter from "./routes/hearings.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://easy-case.vercel.app"],
+    origin: ["http://localhost:5173", "https://easy-case.vercel.app", "http://3.229.137.7"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
