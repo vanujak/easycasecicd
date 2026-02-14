b5db710608fa489783bfc9f5ac5ca47bc7090ad8
vanujak <vanujakofficial@gmail.com>
2026-02-14 16:20:32 +0530

    CICD test

--- frontend/src/components/NavbarHome.jsx
+++ frontend/src/components/NavbarHome.jsx
@@ -69,7 +69,6 @@ export default function NavbarHome() {
           <NavLink to="/login" className={cls}>Log in</NavLink>
           <NavLink to="/signup" className={cls}>Sign up</NavLink>
           <NavLink to="/contact" className={cls}>Contact Us</NavLink>
-          <NavLink to="/contact" className={cls}>5131-Vanuja</NavLink>
         </div>
       </div>
 
